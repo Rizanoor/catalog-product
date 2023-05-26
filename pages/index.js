4511a290c3c97a9654204474c354c846d181ad1c
Rizanoor <rizanoorfauzan123@gmail.com>
2023-05-27 06:05:12 +0700

    feat: productCard component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,34 @@
 import React from "react";
 import Layout from "@common/components/Layout";
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import ProductCard from '@common/components/ProductCard';
 
 const HomePage = ({ products }) => {
   const { list } = products;
 
   return (
     <Layout>
-      <div>
-          {list.map((list) => {
-            return <p key={list.id}>
-              {list.name}</p>;
-          })}
-      </div>
-    </Layout>
+    <Container maxWidth="sm">
+      <Grid container spacing={2}>
+        {list.map(list => {
+          return (
+            <Grid key={list.id} item xs={6} sm={6}>
+              <ProductCard
+                productID={list.id}
+                img={list.img}
+                title={list.name}
+                promoLabel={list.promo}
+                price={list.price}
+                rating={list.rating}
+                sold={list.sold}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  </Layout>
   );
 };
 
